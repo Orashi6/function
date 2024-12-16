7aae28eabb3c5aec37c5ee13bc5260fac62a7d63
Orashi6 <bekb0323@gmail.com>
2024-12-16 21:19:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,123 @@
+
+
+function light() {
+    const body = document.querySelector('body')
+    const h1 = document.querySelector('h1')
+    const p = document.querySelector('p')
+    const a = document.querySelector('a')
+    const a1 = document.getElementById('a1')
+    const a2 = document.getElementById('a2')
+    const a3 = document.getElementById('a3')
+    const item = document.getElementById('item')
+    const button = document.querySelector('button')
+
+
+    h1.style.color = 'black'
+    p.style.color = 'black'
+    a.style.color = 'black'
+    a1.style.color = 'black'
+    a2.style.color = 'black'
+    a3.style.color = 'black'
+    item.style.color = 'black'
+    button.style.color = 'black'
+    body.style.backgroundColor = ' #C4B5A5'
+    
+}
+function dark() {
+    const body = document.querySelector('body')
+    const h1 = document.querySelector('h1')
+    const p = document.querySelector('p')
+    const a = document.querySelector('a')
+    const a1 = document.getElementById('a1')
+    const a2 = document.getElementById('a2')
+    const a3 = document.getElementById('a3')
+    const item = document.getElementById('item')
+    const button = document.querySelector('button')
+
+
+    h1.style.color = 'white'
+    p.style.color = 'white'
+    a.style.color = 'white'
+    button.style.color = 'white'
+    a1.style.color = 'white'
+    a2.style.color = 'white'
+    a3.style.color = 'white'
+    item.style.color = 'white'
+    body.style.backgroundColor = 'black'
+}
+
+// function light() {
+//     const body = document.querySelector('body')
+//     const akr1 = document.getElementsByClassName('a1')
+//     const akr2 = document.getElementsByClassName('a2')
+//     const akr3 = document.getElementsByClassName('a3')
+//     const logo = document.getElementsByClassName('logo')
+//     const item = document.getElementsByClassName('item')
+//     const p = document.querySelector('p')
+//     const button = document.querySelector('button')
+//     body.append(akr1,akr2,akr3,logo,item,p,button)
+
+//     body.style.backgroundColor  = ' #C4B5A5'
+//     akr1.style.Color = 'black'
+//     akr2.style.Color = 'black'
+//     akr3.style.Color = 'black'
+//     item.style.color = 'black'
+//     p.style.Color = 'black'
+//     button.style.Color = 'black'
+    
+    
+// }
+
+// function dark() {
+//     const body = document.querySelector('body')
+//     const akr1 = document.querySelector('a1')
+//     const akr2 = document.querySelector('a2')
+//     const akr3 = document.querySelector('a3')
+//     const logo = document.querySelector('logo')
+//     const item = document.querySelector('item')
+//     const p = document.querySelector('p')
+//     const button = document.querySelector('button')
+//     body.append(akr1,akr2,akr3,logo,item,p,button)
+
+//     body.style.backgroundColor = 'black'
+//     akr1.style.Color = 'white'
+//     akr2.style.Color = 'white'
+//     akr3.style.Color = 'white'
+//     item.style.color = 'white'
+//     p.style.Color = 'white'
+//     button.style.Color = 'white'
+// }
+
+
+// const body = document.querySelector('body');
+// const a1 = document.querySelector('a1');
+// const a2 = document.querySelector('a2');
+// const a3 = document.querySelector('a3');
+// const logo = document.querySelector('logo');
+// const p = document.querySelector('p');
+// const button = document.querySelectorAll('button');
+
+
+// function light() {
+//     body.style.backgroundColor = '#C4B5A5';
+//     a1.
+//     if (item) {
+//       item.style.color = 'black';
+//     }
+//     p.style.color = 'black';
+//     button.style.color = 'black';
+//   }
+  
+//   function dark() {
+//     body.style.backgroundColor = 'black';
+//     a1.style.color = 'white';
+//     a2.style.color = 'white';
+//     a3.style.color = 'white';
+
+//     if (item) {
+//       item.style.color = 'white';
+//     }
+//     p.style.color = 'white';
+//     button.style.color = 'white';
+//   }
+  
